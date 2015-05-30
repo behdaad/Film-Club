359d2dbe030e16d4153f2200a0ca16fbed8e8dd5
behdad-keynejad <behdad.keynejad@gmail.com>
2015-05-30 18:27:37 +0430

    Models going further-er.

--- FilmClub/admin.py
+++ FilmClub/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Movie, Post, Like, Comment
+from .models import Movie, Post, Like, Comment, ExtendedUser
 
 admin.site.register(Movie)
 admin.site.register(Post)
 admin.site.register(Like)
 admin.site.register(Comment)
+admin.site.register(ExtendedUser)
--- FilmClub/models.py
+++ FilmClub/models.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User
 class Movie(models.Model):
     name = models.CharField(max_length=127)
     description = models.TextField()
+    genre = models.CharField(max_length=127)
     director = models.CharField(max_length=127)
     writer = models.CharField(max_length=127)
     actor1 = models.CharField(max_length=127)
@@ -16,6 +17,9 @@ class Movie(models.Model):
     actor3 = models.CharField(max_length=127)
     runningMinutes = models.IntegerField()
     releaseDate = models.DateField()
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -25,14 +29,34 @@ class Post(models.Model):
     movie = models.ForeignKey(Movie)
     review = models.TextField()
 
+    def __str__(self):
+        return self.author + ' (' + self.movie + ')'
+
 
 class Like(models.Model):
     user = models.ForeignKey(User)
     post = models.ForeignKey(Post)
     date = models.DateTimeField(default=datetime.datetime.now)
 
+    def __str__(self):
+        return self.user + " liked " + str(self.post)
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User)
     post = models.ForeignKey(Post)
     date = models.DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return self.user + " commented on " + str(self.post)
+
+
+class ExtendedUser(models.Model):
+    user = models.OneToOneField(User)
+    displayName = models.CharField(max_length=127)
+    birthday = models.DateField()
+    isMale = models.BinaryField()
+    following = models.ManyToManyField('self', symmetrical=False, blank=True)
+
+    def __str__(self):
+        return self.user.username
